02d5b3e0740788313ffa0689deb40e021a391b12
Yukifumi <tkhsh.shmd.ykfm.4296@gmail.com>
2020-09-22 17:42:31 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    private float visiblePosZ = -6.5f;
+
+    private GameObject gameoverText;
+
+    private GameObject scoreText;
+
+    private int morescore = 0;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        this.scoreText = GameObject.Find("Score");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+        this.scoreText.GetComponent<Text>().text = "Score:" + morescore;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            morescore += 10;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            morescore += 20;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            morescore += 40;
+        }
+    }
+
+
+
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+
+    public Text scoreText;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        SetScore();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "SmallStarTag")
+        {
+            score += 150;
+        }
+        else
+        {
+            score += 100;
+        }
+
+        SetScore();
+    }
+
+
+    void SetScore()
+    {
+        scoreText.text = string.Format("Score:{0}", score);
+    }
+
+   
+}
